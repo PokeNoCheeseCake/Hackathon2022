7812a0f4932e6b8982228aa832019ef37efa5f42
Ariel Bashari <arielbashari464@gmail.com>
2022-04-10 14:49:03 +0300

    Ariel B. - Car can turn now

--- car.py
+++ car.py
@@ -1,9 +1,13 @@
+from utils import scale_image, blit_rotate_center
+
+
 class Car(object):
-    def __init__(self, img, position, angle):
+    def __init__(self, img, position, angle, rotation_val):
         self.img = img
         self.speed = Vector(0.0, 0.0)
         self.position = Vector(position.get_x(), position.get_y())
         self.angle = angle
+        self.rotation_val = rotation_val
 
     def accelerate(self, vector):
         self.speed.set_x(self.speed.get_x() + vector.get_x())
@@ -12,6 +16,15 @@ class Car(object):
     def move(self):
         self.position.set_x(self.position.get_x() + self.speed.get_x())
         self.position.set_y(self.position.get_y() + self.speed.get_y())
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_val
+        elif right:
+            self.angle -= self.rotation_val
+
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.position.get_x(), self.position.get_y()), self.angle)
 
 
 class Vector(object):
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-
 import pygame
 
 from car import Car
@@ -12,7 +10,7 @@ pygame.display.set_caption('Rush Hour')
 clock = pygame.time.Clock()
 
 img_path = 'images/red_car.png'
-car = Car(pygame.image.load(img_path), Vector(0.0, 0.0), 0.0)
+car = Car(pygame.image.load(img_path), Vector(0.0, 0.0), 0.0, 1)
 car.accelerate(Vector(1, 1))
 
 screen = pygame.display.set_mode((800, 600))
@@ -21,8 +19,9 @@ running = True
 while running:
 
     screen.fill((0, 0, 0))
-    screen.blit(car.img, (car.position.get_x(), car.position.get_y()))
+    car.rotate(left=True)
     car.move()
+    car.draw(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
