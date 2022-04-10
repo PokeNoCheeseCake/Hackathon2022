ed410f62bfd3ace848f7a6f5700dd305a6b627eb
מתן חומרי <mhomri@g.jct.ac.il>
2022-04-10 16:36:36 +0300

    1. add 2 images to the images' dir 2. main.py file refactor 3. add MenuButton file

--- main.py
+++ main.py
@@ -1,34 +1,90 @@
-import random
-
-import pygame
-
+import pygame, sys
 from car import Car
 from car import Vector
+from MenuButton import Button
+
 
 pygame.init()
 
-pygame.display.set_caption('Rush Hour')
+# assigning values to X and Y variable
+X = 400
+Y = 400
 
+# create the display surface object
+# of specific dimension..e(X, Y).
+display_surface = pygame.display.set_mode((X, Y))
+
+# create a font object.
+# 1st parameter is the font file
+# which is present in pygame.
+# 2nd parameter is size of the font
+font = pygame.font.Font('freesansbold.ttf', 32)
 clock = pygame.time.Clock()
-
-img_path = 'images/red_car.png'
-car = Car(pygame.image.load(img_path), Vector(0.0, 0.0), 0.0)
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Rush Hour')
+img_path = 'images/'
+car = Car(pygame.image.load(img_path+"red_car.png"), Vector(0.0, 0.0), 0.0)
 car.accelerate(Vector(1, 1))
 
 screen = pygame.display.set_mode((800, 600))
 
-running = True
-while running:
+BG = pygame.image.load(img_path+"car.png")
 
-    screen.fill((0, 0, 0))
-    screen.blit(car.img, (car.position.get_x(), car.position.get_y()))
-    car.move()
+def play():
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    running = True
+    while running:
 
-    # Draws the surface object to the screen.
-    pygame.display.update()
+        screen.fill((0, 0, 0))
+        screen.blit(car.img, (car.position.get_x(), car.position.get_y()))
+        car.move()
 
-    clock.tick(40)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Draws the surface object to the screen.
+        pygame.display.update()
+        clock.tick(40)
+
+def options():
+    pass
+
+def displayMenu():
+    while True:
+        screen.blit(BG, (0, 0))
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+        MENU_TEXT = font.render('Welcome!', True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load(img_path+"Play Rect.png"), pos=(400, 200),
+                             text_input="PLAY", font=font, base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load(img_path+"Play Rect.png"), pos=(400, 350),
+                                text_input="or yarok", font=font, base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load(img_path+"Play Rect.png"), pos=(400, 500),
+                             text_input="ABOUT", font=font, base_color="#d7fcd4", hovering_color="White")
+
+        screen.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pass
+                    # pygame.quit()
+                    # sys.exit()
+
+        pygame.display.update()
+
+
+displayMenu()
