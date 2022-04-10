449f14d89492c6a93f9d63ef6e8de3337fc2a237
מתן חומרי <mhomri@g.jct.ac.il>
2022-04-10 15:24:08 +0300

    Fix(main): refactored main file and created main menu

--- HackathonCarGame/main.py
+++ HackathonCarGame/main.py
@@ -1,16 +1,7 @@
-import pygame;
+import pygame, sys
+from MenuButton import Button
 
-pygame.init();
-
-# hehe - itay also laughed
-#hi
-pygame.display.set_caption('אין מנוס');
-
-# define the RGB value for white,
-#  green, blue colour .
-white = (255, 255, 255)
-green = (0, 255, 0)
-blue = (0, 0, 128)
+pygame.init()
 
 # assigning values to X and Y variable
 X = 400
@@ -28,25 +19,64 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 
 # create a text surface object,
 # on which text is drawn on it.
-text = font.render('You make me proud son', True, green, blue)
+#text = font.render('You make me proud son', True, green, blue)
 
 # create a rectangular object for the
 # text surface object
-textRect = text.get_rect()
+#textRect = text.get_rect()
 
 # set the center of the rectangular object.
-textRect.center = (X // 2, Y // 2)
+#textRect.center = (X // 2, Y // 2)
+
+screen = pygame.display.set_mode((800, 600))
+
+# running = True
+
+BG = pygame.image.load("C:\MyPrograming\hakaton\gradient.png")
+
+def play():
+    pass
+
+def options():
+    pass
+
+def displayMenu():
+    pygame.display.set_caption("Menu")
+    while True:
+        screen.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = font.render('Welcome!', True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("C:\MyPrograming\hakaton\Play Rect.png"), pos=(400, 200),
+                             text_input="PLAY", font=font, base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("C:\MyPrograming\hakaton\Play Rect.png"), pos=(400, 350),
+                                text_input="or yarok", font=font, base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("C:\MyPrograming\hakaton\Play Rect.png"), pos=(400, 500),
+                             text_input="about", font=font, base_color="#d7fcd4", hovering_color="White")
+
+        screen.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
 
 
-screen = pygame.display.set_mode((800, 600));
-
-running = True;
-while running:
-    display_surface.blit(text, textRect)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False;
-
-    # Draws the surface object to the screen.
-    pygame.display.update()
+displayMenu()
